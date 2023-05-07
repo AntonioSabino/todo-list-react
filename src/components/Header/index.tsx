58eec9f903bedd20356c104b5597a90a7f52814e
Antonio Sabino <aslimajr@outlook.com>
2023-05-06 23:50:31 -0300

    feat: criar componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/logo.svg';
+import styles from './styles.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt=" ToDo logo" />
+    </header>
+  )
+}
